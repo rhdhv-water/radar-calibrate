15ddd40232f6f8f3144090ade354633f86853be1
jonnekleijer <jonne.kleijer@rhdhv.com>
2017-05-18 08:25:19 +0200

    run tests from test file instead

--- tests/test_kriging.py
+++ tests/test_kriging.py
@@ -2,36 +2,115 @@
 # -*- coding: utf-8 -*-
 # Jonne Kleijer, Royal HaskoningDHV
 
-"""
-# IN:
-len(x) = 71 NPArray
-len(y) = 71 NPArray
-len(z) = 71 NPArray
-len(radar) = 71 NPArray
-len(xi) = 245000 NPArray
-len(yi) = 245000 NPArray
-len(zi) = 245000 NPArray
+#==============================================================================
+# Main
+#==============================================================================
+import h5py
+import json
 
-# OUT:
-shape(rain_est) = 490 (r) * 500 (c)
-     product_corners = -110000,210000,-110000,700000,390000,700000,390000,210000
-"""
-
+import numpy
+import matplotlib.pyplot as plt
 
 import os
-import numpy
-import pandas
+from time import time
+import logging
+from calibrate import kriging
+plt.close('all')
 
-root = r'C:\Project_OG\BA8186_NRR\2_technical\radar-calibrate'
-file = '\data\grounddata2.json'
-os.chdir(root)
+#==============================================================================
+# INPUT
+#==============================================================================
+root = r'C:\Project_OG\BA8186_NRR\2_technical'
+file_station = r'\radar-calibrate\data\2017_grounddata.json'
+file_aggregate = r'\radar-calibrate\data\24uur_20170223080000.h5'
+file_calibrate = r'\radar-calibrate\data\RAD_TF2400_U_20170223080000.h5'
+date = '2017-02-23T08:00:00'
 
-pandas.read_json(root + file)
+#==============================================================================
+# Read files
+#==============================================================================
+with open(os.path.join(root + file_station)) as json_data:
+    data = json.load(json_data)
+with h5py.File(os.path.join(root + file_aggregate), 'r') as ds:
+    aggregate = numpy.float64(ds['precipitation'][:]).T # Index is [x][y]
+    grid_extent = ds.attrs['grid_extent']
+    grid_size = ds.attrs['grid_size']
+    left, right, top, bottom = grid_extent
+    pixelwidth = (right - left) / grid_size[0]
+    pixelheight = (bottom - top) / grid_size[1]
+with h5py.File(os.path.join(root + file_calibrate), 'r') as ds:
+    calibrate = numpy.float64(ds['image1/image_data']).T # Index is [x][y]
+    coords = numpy.array(ds.attrs['cal_station_coords'])
+    x = coords[:,0]
+    y = coords[:,1]
+    z = numpy.array(ds.attrs['cal_station_measurements'])
 
+#==============================================================================
+# Prep data as numpy arrays, as the required format of the krige modules
+#==============================================================================
+rainstation = numpy.array(data[date])
+radar = kriging.get_radar_for_locations(x, y, grid_extent, aggregate, block=2)
+numpy.array(radar)
+xi, yi = kriging.get_grid(aggregate, grid_extent)
+zi = aggregate.flatten()
 
-#rainstation = pd.read_csv(root + file)
-#x = rainstation['x']
-#y = rainstation['y']
-#z = rainstation['z']
+#==============================================================================
+# # Run the KED functions
+#==============================================================================
+tic = time()
+rain_est_R, correction_factor = kriging.ked_R(x, y, z, radar, xi, yi, zi)
+calibrate_R = rain_est_R.reshape([500, 490])
+correction_factor = correction_factor.reshape([500, 490])
+logging.info("It took R {} seconds to complete ked with a grid of 1000m".format(time() - tic))
 
+tic = time()
+rain_est_py = kriging.ked_py(x, y, z, radar, xi, yi, zi)
+calibrate_py = rain_est_py.reshape([500,490])
+logging.info("It took py {} seconds to complete ked with a grid of 1000m".format(time() - tic))
+
+#==============================================================================
+# VISUALISATION
+#==============================================================================
+#Plot error of rainstations vs radar in (mm)
+plt.figure()
+plt.scatter(z, radar)
+plt.plot([0, 40], [0, 40],'k')
+plt.xlabel('$P_{station}\/(mm)$')
+plt.ylabel('$P_{radar}\/(mm)$')
+plt.axis([0, 40, 0, 40])
+plt.show()
+
+# Plot radar_calibrate_R
+plt.figure()
+f221 = plt.subplot(2, 2, 1)
+plt.imshow(aggregate, cmap='rainbow', vmin=0, vmax=40)
+plt.ylabel('y-coordinate')
+plt.title('aggregate')
+f222 = plt.subplot(2, 2, 2, sharex=f221, sharey=f221)
+plt.imshow(calibrate/100, cmap='rainbow', vmin=0, vmax=40)
+plt.title('$calibrate_{original}$')
+f223 = plt.subplot(2, 2, 3, sharex=f221, sharey=f221)
+plt.imshow(calibrate_R, cmap='rainbow', vmin=-40, vmax=40)
+plt.xlabel('x-coordinate')
+plt.ylabel('y-coordinate')
+plt.title('$calibrate_R$')
+plt.subplot(2, 2, 4, sharex=f221, sharey=f221)
+plt.imshow(calibrate_py, cmap='rainbow', vmin=-40, vmax=40)
+plt.xlabel('x-coordinate')
+plt.title('$calibrate_{py}$')
+plt.tight_layout()
+plt.show()
+
+# Plot histgram error
+plt.figure()
+plt.subplot(2,1,1)
+plt.hist(calibrate_R.flatten() - calibrate.flatten(), bins = range(-10,10,1))
+plt.xlim([-10, 10])
+plt.title('$histogram\/\/of\/\/error\/\/ked_R$')
+plt.subplot(2,1,2)
+plt.hist(calibrate_py.flatten() - calibrate.flatten(), bins = range(-10,10,1))
+plt.xlim([-10, 10])
+plt.title('$histogram\/\/of\/\/error\/\/ked_{py}$')
+plt.tight_layout()
+plt.show()
 
