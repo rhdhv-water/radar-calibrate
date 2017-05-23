87d3c30bdf5aca54bcf6ecf98784d79c21b70c0c
jonnekleijer <jonne.kleijer@rhdhv.com>
2017-05-23 16:45:38 +0200

    move tests file to test folder

--- tests/test_kriging.py
+++ tests/test_kriging.py
@@ -2,44 +2,32 @@
 # -*- coding: utf-8 -*-
 # Jonne Kleijer, Royal HaskoningDHV
 
-#==============================================================================
-# Main
-#==============================================================================
 import h5py
 import numpy
 import matplotlib.pyplot as plt
-
 import os
 from time import time
 
-# Debug
-import sys
-import importlib
-if "kriging" not in sys.modules:
-    import kriging
-else: 
-    importlib.reload(kriging)
-    
+from calibrate import kriging
 plt.close('all')
 
 #==============================================================================
 # INPUT
 #==============================================================================
-root = r'C:\Project_OG\BA8186_NRR\2_technical'
-file_aggregate = r'\radar-calibrate\data\24uur_20170223080000.h5'
-file_calibrate = r'\radar-calibrate\data\RAD_TF2400_U_20170223080000.h5'
+file_aggregate = r'..\data\24uur_20170223080000.h5'
+file_calibrate = r'..\data\RAD_TF2400_U_20170223080000.h5'
 
 #==============================================================================
 # Read files
 #==============================================================================
-with h5py.File(os.path.join(root + file_aggregate), 'r') as ds:
+with h5py.File(os.path.join(file_aggregate), 'r') as ds:
     aggregate = numpy.float64(ds['precipitation'][:]).T # Index is [x][y]
     grid_extent = ds.attrs['grid_extent']
     grid_size = ds.attrs['grid_size']
     left, right, top, bottom = grid_extent
     pixelwidth = (right - left) / grid_size[0]
     pixelheight = (bottom - top) / grid_size[1]
-with h5py.File(os.path.join(root + file_calibrate), 'r') as ds:
+with h5py.File(os.path.join(file_calibrate), 'r') as ds:
     calibrate = numpy.float64(ds['image1/image_data']).T # Index is [x][y]
     coords = numpy.array(ds.attrs['cal_station_coords'])
     x = coords[:, 0]
@@ -56,7 +44,7 @@ yi = numpy.float32(yi).flatten()
 zi = aggregate.flatten()
 
 #==============================================================================
-# # Run the KED functions
+# RUN FUNCTIONS
 #==============================================================================
 tic = time()
 rain_est_R = kriging.ked_R(x, y, z, radar, xi, yi, zi, False)
