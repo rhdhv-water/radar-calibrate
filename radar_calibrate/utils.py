6627ee20ce0ae0a7775d44452f1279a3b873b9ce
tomvansteijn <tom.van.steijn@rhdhv.com>
2017-05-27 21:35:17 +0200

    added utils.py; function time decorator

---
+++ radar_calibrate/utils.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""Summary
+"""
+# Royal HaskoningDHV
+
+from collections import namedtuple
+import time
+
+Timedresult = namedtuple('timedresult', 'dt', 'result')
+
+
+def timeit(func):
+    """Function or method decorator measuring execution time
+
+    See https://www.andreas-jung.com/contents/a-python-decorator-for-measuring-the-execution-time-of-funcs
+
+    Parameters
+    ----------
+    func : Callable
+        Function or method to be timed
+
+    Returns
+    -------
+    Timedresult
+        Named tuple containing execution time and result
+    """
+    def timed(*args, **kwargs):
+        tic = time.time()
+        result = func(*args, **kwargs)
+        toc = time.time()
+        dt = tic - toc
+        return Timedresult(dt=dt, result=result)
+
+    return timed
