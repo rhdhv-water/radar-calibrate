513e68024f0e458df2ac677eae8b342350ee9ba3
tomvansteijn <tom.van.steijn@rhdhv.com>
2017-05-28 23:06:18 +0200

    added test for rainstations2shape

--- radar_calibrate/tests/test_files.py
+++ radar_calibrate/tests/test_files.py
@@ -9,6 +9,7 @@ Run using pytest:
 
 from radar_calibrate import config
 from radar_calibrate import files
+from radar_calibrate import utils
 
 import numpy
 
@@ -26,3 +27,19 @@ def test_hdf2raster():
         name, ext = os.path.splitext(filename)
         rasterfile = os.path.join(config.RASTERDIR, name + '.tif')
         files.hdf2raster(h5file, rasterfile)
+
+
+def test_rainstations2shape():
+    h5files = [
+        (r'data\24uur_20170223080000.h5', r'data\RAD_TF2400_U_20170223080000.h5'),
+        (r'data\24uur_20170228080000.h5', r'data\RAD_TF2400_U_20170228080000.h5'),
+        (r'data\24uur_20170305080000.h5', r'data\RAD_TF2400_U_20170305080000.h5'),
+        ]
+    for aggregatefile, calibratefile in h5files:
+        logging.info('writing {} to shapefile'.format(
+            os.path.basename(calibratefile)))
+        filename = os.path.basename(calibratefile)
+        name, ext = os.path.splitext(filename)
+        shapefile = os.path.join(config.SHAPEDIR, name + '.shp')
+        files.rainstations2shape(aggregatefile, calibratefile, shapefile,
+            agg=utils.safe_first)
