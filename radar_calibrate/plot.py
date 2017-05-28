0c7268519a28843d93a5df9eee4e4fb59f792553
tomvansteijn <tom.van.steijn@rhdhv.com>
2017-05-28 23:12:21 +0200

    update compare_ked; added bg_shape

--- radar_calibrate/plot.py
+++ radar_calibrate/plot.py
@@ -3,6 +3,7 @@
 
 """
 # Royal HaskoningDHV
+from radar_calibrate import files
 
 import matplotlib.pyplot as plt
 
@@ -14,7 +15,7 @@ def vgm_r(vgm_py, residual_py):
     plt.title('R variogram')
     return figure
 
-def plot_error(z, radar):
+def error(z, radar):
     #Plot error of rainstations vs radar in (mm)
     plt.figure()
     plt.scatter(z, radar)
@@ -25,7 +26,7 @@ def plot_error(z, radar):
     plt.show()
 
 
-def plot_histogram(calibrate, calibrate_r, calibrate_py):
+def histogram(calibrate, calibrate_r, calibrate_py):
     # Plot histgram error
     plt.figure()
     plt.subplot(2, 1, 1)
@@ -44,25 +45,63 @@ def compare_ked(aggregate,
     calibrate, calibrate_r, calibrate_py,
     imagefile=None,
     ):
-    # Plot radar_calibrate_R
-    plt.figure()
-    f221 = plt.subplot(2, 2, 1)
-    plt.imshow(aggregate, cmap='rainbow', vmin=0, vmax=40)
-    plt.ylabel('y-coordinate')
-    plt.title('aggregate')
-    f222 = plt.subplot(2, 2, 2, sharex=f221, sharey=f221)
-    plt.imshow(calibrate, cmap='rainbow', vmin=0, vmax=40)
-    plt.title('$calibrate_{original}$')
-    f223 = plt.subplot(2, 2, 3, sharex=f221, sharey=f221)
-    plt.imshow(calibrate_r, cmap='rainbow', vmin=0, vmax=40)
-    plt.xlabel('x-coordinate')
-    plt.ylabel('y-coordinate')
-    plt.title('$calibrate_R$')
-    plt.subplot(2, 2, 4, sharex=f221, sharey=f221)
-    plt.imshow(calibrate_py, cmap='rainbow', vmin=0, vmax=40)
-    plt.xlabel('x-coordinate')
-    plt.title('$calibrate_{py}$')
-    plt.tight_layout()
+
+    fig, axes  = plt.subplots(nrows=3, ncols=2,
+        figsize=(8.27, 11.7), sharex=True, sharey=True)
+
+    axes[0, 0].imshow(aggregate, cmap='inferno', vmin=0, vmax=40, alpha=0.8)
+    axes[0, 0].set_title('$aggregate$')
+    axes[0, 0].grid(linestyle= '--', linewidth=0.5)
+    axes[0, 0].xaxis.set_ticklabels([])
+    axes[0, 0].yaxis.set_ticklabels([])
+    for x, y in files.read_bg_shape():
+        axes[0, 0].plot(x, y, color='skyblue', linewidth=0.5)
+
+    axes[0, 1].imshow(calibrate, cmap='inferno', vmin=0, vmax=40, alpha=0.8)
+    axes[0, 1].set_title('$calibrate_{original}$')
+    axes[0, 1].grid(linestyle= '--', linewidth=0.5)
+    axes[0, 1].xaxis.set_ticklabels([])
+    axes[0, 1].yaxis.set_ticklabels([])
+    for x, y in files.read_bg_shape():
+        axes[0, 1].plot(x, y, color='skyblue', linewidth=0.5)
+
+    axes[1, 0].imshow(calibrate_r, cmap='inferno', vmin=0, vmax=40, alpha=0.8)
+    axes[1, 0].set_title('$calibrate_R$')
+    axes[1, 0].grid(linestyle= '--', linewidth=0.5)
+    axes[1, 0].xaxis.set_ticklabels([])
+    axes[1, 0].yaxis.set_ticklabels([])
+    for x, y in files.read_bg_shape():
+        axes[1, 0].plot(x, y, color='skyblue', linewidth=0.5)
+
+    axes[1, 1].imshow(calibrate_r - calibrate, cmap='inferno', vmin=0, vmax=40, alpha=0.8)
+    axes[1, 1].set_title('$calibrate_R - calibrate_{original}$')
+    axes[1, 1].grid(linestyle= '--', linewidth=0.5)
+    axes[1, 1].xaxis.set_ticklabels([])
+    axes[1, 1].yaxis.set_ticklabels([])
+    for x, y in files.read_bg_shape():
+        axes[1, 1].plot(x, y, color='skyblue', linewidth=0.5)
+
+    axes[2, 0].imshow(calibrate_py, cmap='inferno', vmin=0, vmax=40, alpha=0.8)
+    axes[2, 0].set_title('$calibrate_{py}$')
+    axes[2, 0].grid(linestyle= '--', linewidth=0.5)
+    axes[2, 0].xaxis.set_ticklabels([])
+    axes[2, 0].yaxis.set_ticklabels([])
+    for x, y in files.read_bg_shape():
+        axes[2, 0].plot(x, y, color='skyblue', linewidth=0.5)
+
+    im = axes[2, 1].imshow(calibrate_py - calibrate, cmap='inferno', vmin=0, vmax=40, alpha=0.8)
+    axes[2, 1].set_title('$calibrate_{py} - calibrate_{original}$')
+    axes[2, 1].grid(linestyle= '--', linewidth=0.5)
+    axes[2, 1].xaxis.set_ticklabels([])
+    axes[2, 1].yaxis.set_ticklabels([])
+    for x, y in files.read_bg_shape():
+        axes[2, 1].plot(x, y, color='skyblue', linewidth=0.5)
+
+    fig.subplots_adjust(right=0.92)
+    cbar_ax = fig.add_axes([0.94, 0.67, 0.02, 0.2])
+    fig.colorbar(im, cax=cbar_ax)
+
+    # plt.tight_layout()
     if imagefile is not None:
         plt.savefig(imagefile, bbox_inches='tight')
     else:
