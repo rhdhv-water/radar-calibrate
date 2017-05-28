68d08ec5fe2e9d3d689a06e58ae1df11d7aa78f8
tomvansteijn <tom.van.steijn@rhdhv.com>
2017-05-28 23:11:40 +0200

    added calibrate_kwargs, read_bg_shape, rainstations2shapefile

--- radar_calibrate/files.py
+++ radar_calibrate/files.py
@@ -5,9 +5,11 @@ To be merged with openradar.gridtools.
 
 """
 # Royal HaskoningDHV
+from radar_calibrate import config
 from radar_calibrate import gridtools
 
 from affine import Affine
+from fiona.crs import from_epsg
 import rasterio
 import numpy
 import fiona
@@ -71,7 +73,6 @@ def get_testdata(aggregatefile, calibratefile):
     cal_station_coords = cal_station_coords[rain_ge_zero, :]
     cal_station_values = cal_station_values[rain_ge_zero]
 
-
     # sample aggregate at calibration station coordinates
     radar_values = gridtools.sample_grid(
         coords=cal_station_coords,
@@ -95,8 +96,19 @@ def get_testdata(aggregatefile, calibratefile):
     xi, yi = [numpy.float32(a).flatten() for a in basegrid.get_grid()]
     zi = aggregate.flatten()
 
+    # calibrate kwargs to dict
+    calibrate_kwargs = {
+        'x': x,
+        'y': y,
+        'z': z,
+        'radar': radar,
+        'xi': xi,
+        'yi': yi,
+        'zi': zi,
+        }
+
     # return tuple
-    return x, y, z, radar, xi, yi, zi, aggregate, calibrate
+    return calibrate_kwargs, aggregate, calibrate
 
 
 def hdf2raster(h5file, rasterfile,
@@ -184,3 +196,128 @@ def write_raster(array, rasterfile, transform,
     logging.debug('writing to {}'.format(os.path.basename(rasterfile)))
     with rasterio.open(rasterfile, 'w', **profile) as dst:
         dst.write(array.astype(dtype), 1)
+
+
+def rainstations2shape(aggregatefile, calibratefile, shapefile,
+    calibrate=None, epsg=28992, driver='ESRI Shapefile', agg=numpy.median):
+    """Summary
+
+    Parameters
+    ----------
+    aggregatefile : TYPE
+        Description
+    calibratefile : TYPE
+        Description
+    shapefile : TYPE
+        Description
+    calibrate : None, optional
+        Description
+    epsg : int, optional
+        Description
+    driver : str, optional
+        Description
+    """
+    # read aggregate and grid properties from aggregate file
+    with h5py.File(aggregatefile, 'r') as ds:
+        aggregate = get_imagedata(ds)
+        grid_extent = ds.attrs['grid_extent']
+        grid_size = [int(i) for i in ds.attrs['grid_size']]
+
+    # construct basegrid
+    basegrid = gridtools.BaseGrid(extent=grid_extent,
+        size=grid_size)
+
+    # read calibrate and station measurements from calibrate file
+    with h5py.File(calibratefile, 'r') as ds:
+        if calibrate is None:
+            calibrate = get_imagedata(ds)
+        cal_station_coords = ds.attrs['cal_station_coords']
+        cal_station_values = ds.attrs['cal_station_measurements']
+
+    # sample aggregate at calibration station coordinates
+    radar_values = gridtools.sample_grid(
+        coords=cal_station_coords,
+        grid=aggregate,
+        geotransform=basegrid.get_geotransform(),
+        agg=agg,
+        )
+    radar = numpy.array([v for v in radar_values])
+
+    # sample calibrate at calibration station coordinates
+    cal_radar_values = gridtools.sample_grid(
+        coords=cal_station_coords,
+        grid=calibrate,
+        geotransform=basegrid.get_geotransform(),
+        agg=agg,
+        )
+    cal_radar = numpy.array([v for v in cal_radar_values])
+
+    # records
+    zipped_rows = zip(cal_station_coords, cal_station_values, radar, cal_radar)
+    records = ({'geometry': {
+        'coordinates': (x , y),
+        'type': 'Point',
+        },
+        'properties': {
+            'x': x,
+            'y': y,
+            'z': z,
+            'radar': radar,
+            'cal_radar': cal_radar,
+            'dz':  cal_radar - z,
+            }
+        } for (x, y), z, radar, cal_radar in zipped_rows)
+
+    # shapefile coordinate reference
+    crs = from_epsg(epsg)
+
+    # shapefile schema
+    schema = {
+        'geometry': 'Point',
+        'properties': {
+            'x': 'float',
+            'y': 'float',
+            'z': 'float',
+            'radar': 'float',
+            'cal_radar': 'float',
+            'dz': 'float',
+            }
+        }
+
+    # write to file
+    logging.debug('writing to {}'.format(os.path.basename(shapefile)))
+    write_shape(records, shapefile, crs, schema, driver=driver)
+
+
+def write_shape(records, shapefile, crs, schema, driver='ESRI Shapefile'):
+    """Summary
+
+    Parameters
+    ----------
+    records : TYPE
+        Description
+    shapefile : TYPE
+        Description
+    crs : TYPE
+        Description
+    schema : TYPE
+        Description
+    driver : str, optional
+        Description
+    """
+    with fiona.open(shapefile, 'w', driver, schema, crs) as dst:
+        dst.writerecords(records)
+
+
+def read_bg_shape():
+    shapefile = os.path.join(config.MISCDIR, 'nederland_lijn.shp')
+    top = 700000
+    left = -110000
+    with fiona.open(shapefile) as src:
+        for row in src:
+            x, y = zip(*row['geometry']['coordinates'])
+            x = numpy.array(x)
+            y = numpy.array(y)
+            x = (x - left) / 1000.
+            y = (top - y) / 1000.
+            yield x, y
