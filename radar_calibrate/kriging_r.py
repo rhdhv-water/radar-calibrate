90bd9726e1cdcfc18dc91fc11ba978fbede9d45b
tomvansteijn <tom.van.steijn@rhdhv.com>
2017-05-28 07:11:07 +0200

    new copy from openradar.interpolation; wip

--- radar_calibrate/kriging_r.py
+++ radar_calibrate/kriging_r.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 # Royal HaskoningDHV
-from radar_calibrate import vis
 
 import rpy2.robjects as robj
 import numpy
@@ -8,7 +7,7 @@ import numpy
 import logging
 
 
-def ked_r(x, y, z, radar, xi, yi, zi, vario=False):
+def ked(x, y, z, radar, xi, yi, zi):
     """
     Run the kriging method using the R module "gstat".
     Kriging External Drift (or universal kriging).
@@ -22,45 +21,52 @@ def ked_r(x, y, z, radar, xi, yi, zi, vario=False):
 
     # Modification to prevent singular matrix (Tom)
     radar += (1e-9 * numpy.random.rand(len(radar)))
-    radar = robj.FloatVector(radar)
-    x, y, z = robj.FloatVector(x), robj.FloatVector(y), robj.FloatVector(z)
-    xi, yi, zi = robj.FloatVector(xi), robj.FloatVector(yi), robj.FloatVector(zi)
-    rain_radar_frame = robj.DataFrame({'x': x, 'y': y, 'z': z, 'radar': radar})
-    rain_radar_frame_py = numpy.array(rain_radar_frame)
-    radar_frame = robj.DataFrame({'x': xi, 'y': yi, 'radar': zi})
-    radar_frame_py = numpy.array(radar_frame)
 
-    # Create predictor
-    vgm = robj.r.variogram(robj.r("z~radar"),
+    # variables to R dataframes
+    rain_radar_frame = robj.DataFrame({
+        'x': robj.FloatVector(x),
+        'y': robj.FloatVector(y),
+        'z': robj.FloatVector(z),
+        'radar': robj.FloatVector(radar),
+        })
+
+    radar_frame = robj.DataFrame({
+        'x': robj.FloatVector(xi),
+        'y': robj.FloatVector(yi),
+        'radar': robj.FloatVector(zi),
+        })
+
+    # try kriging
+    try:
+        vgm = robj.r.variogram(robj.r("z~radar"), robj.r('~ x + y'),
+                               data=rain_radar_frame,
+                               cutoff=50000, width=5000)
+        residual_callable = robj.r('fit.variogram')
+        residual = residual_callable(vgm, robj.r.vgm(1, 'Sph', 25000, 1))
+        ked = robj.r('NULL')
+        ked = robj.r.gstat(ked, 'raingauge', robj.r("z~radar"),
                            robj.r('~ x + y'),
                            data=rain_radar_frame,
-                           cutoff=50000,
-                           width=5000)
-    vgm_py = numpy.array(vgm)
-    residual_callable = robj.r('fit.variogram')
-    residual = residual_callable(vgm, robj.r.vgm(1, 'Sph', 25000, 1))
-    residual_py = numpy.array(residual)
-    if vario == True:
-        vis.plot_vgm_R(vgm_py, residual_py).show()
+                           model=residual, nmax=40)
+        result = robj.r.predict(ked, radar_frame, nsim=0)
+        rain_est = numpy.array(result[2])
+        # self.crossval_ked = robj.r('gstat.cv')(ked)
+    except:
+        logging.exception('Exception during kriging:')
+        rain_est = zi
 
-    ked = robj.r('NULL')
-    ked = robj.r.gstat(ked, 'raingauge', robj.r("z~radar"),
-                       robj.r('~ x + y'),
-                       data=rain_radar_frame,
-                       model=residual, nmax=40)
-
-    # Run predictor
-    result = robj.r.predict(ked, radar_frame, nsim=0)
-    rain_est = numpy.array(result[2])
-
-    # Correction 0's and extreme kriged values
-    zi = numpy.array(zi)
+    # handle extreme outcomes of kriging
     zero_or_no_data = numpy.logical_or(zi == 0, zi == -9999)
     correction_factor = numpy.ones(zi.shape)
-    correction_factor[~zero_or_no_data] = (rain_est[~zero_or_no_data] / zi[~zero_or_no_data])
-    leave_uncalibrated = numpy.logical_or(correction_factor < 0, correction_factor > 10)
-    logging.info('Leaving {} extreme pixels uncalibrated.'.format(leave_uncalibrated.sum(),))
-#    rain_est[leave_uncalibrated] = zi[leave_uncalibrated]
+    correction_factor[~zero_or_no_data] = (
+        rain_est[~zero_or_no_data] / zi[~zero_or_no_data]
+    )
+    leave_uncalibrated = numpy.logical_or(
+        correction_factor < 0, correction_factor > 10
+    )
+    logging.info('Leaving {} extreme pixels uncalibrated.'.format(
+        leave_uncalibrated.sum(),
+    ))
+    rain_est[leave_uncalibrated] = zi[leave_uncalibrated]
 
-    leave_uncalibrated = leave_uncalibrated.reshape([500, 490])
     return rain_est
