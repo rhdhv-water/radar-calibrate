6e6b0d57e09d2f7bb9326ef4cc3da2acc0d30a6b
tomvansteijn <tom.van.steijn@rhdhv.com>
2017-05-28 23:10:39 +0200

    added safe_first and apply_countrymask

--- radar_calibrate/utils.py
+++ radar_calibrate/utils.py
@@ -2,9 +2,13 @@
 """Summary
 """
 # Royal HaskoningDHV
+from radar_calibrate import config
+
+import h5py
 
 from collections import namedtuple
 import time
+import os
 
 Timedresult = namedtuple('timedresult', 'dt result')
 
@@ -32,3 +36,22 @@ def timethis(func):
         return Timedresult(dt=dt, result=result)
 
     return timed
+
+
+def safe_first(array):
+    try:
+        return array.flatten()[0]
+    except IndexError:
+        return numpy.nan
+
+countrymask_path = os.path.join(config.MISCDIR, 'countrymask.h5')
+with h5py.File(countrymask_path, 'r') as ds:
+    mask = ds['mask'][:]
+
+def apply_countrymask(calibrate, aggregate):
+    """
+    Get a prefabricated country mask, 1 everywhere in the country and 0
+    50 km outside the country. If extent and cellsize are not as in
+    config.py, this breaks.
+    """
+    return mask * calibrate + (1 - mask) * aggregate
