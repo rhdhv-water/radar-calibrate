75b27ab747a7e0b710b6700a68bab7fb8dc130ef
jonnekleijer <jonne.kleijer@rhdhv.com>
2017-06-14 10:13:44 +0200

    test, although not finished

--- radar_calibrate/tests/test_bootstrap.py
+++ radar_calibrate/tests/test_bootstrap.py
@@ -1,2 +1,19 @@
 # -*- coding: utf-8 -*-
 # Royal HaskoningDHV
+
+from radar_calibrate.tests import testconfig
+from radar_calibrate.calibration import ked
+from radar_calibrate.bootstrap import BootStrappedCalibrator
+
+import numpy as np
+import os
+
+aggregatefile = r'24uur_20170223080000.h5'
+calibratefile = r'RAD_TF2400_U_20170223080000.h5'
+aggregatefile = os.path.join(testconfig.DATADIR, aggregatefile)
+calibratefile = os.path.join(testconfig.DATADIR, calibratefile)
+cal = BootStrappedCalibrator(
+    aggregatefile=aggregatefile,
+    calibratefile=calibratefile,
+    )
+result = cal.bootstrap_single(ked)
